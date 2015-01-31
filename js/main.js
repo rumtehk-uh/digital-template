b3a75c38a07068a6c92876d243189b058f714650
zLaine <rumtehk_uh@yahoo.com>
2015-01-31 01:26:22 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ window.onload = function() {
     function preload() {
         // Load an image and call it 'logo'.
     //    game.load.image( 'logo', 'assets/phaser.png' );
-        game.load.spritesheet( 'girlChar', 'assets/GirlLightExample.png', 40, 40, 12 );
+        game.load.spritesheet( 'girlChar', 'assets/GirlLightExample.png', 40, 40, 3 );
     }
     
     var bouncy;
