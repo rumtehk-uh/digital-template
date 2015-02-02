fab820661d15cf1c57f36316f658a102b43abbec
zLaine <rumtehk_uh@yahoo.com>
2015-02-02 04:37:57 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -17,6 +17,7 @@ window.onload = function()
     var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
     var girl;
+    var platforms;
     
     function preload() 
     {
