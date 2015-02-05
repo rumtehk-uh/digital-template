5df4cfff054ed09eacb5d5d95c1ee76d0ebf08a5
zLaine <rumtehk_uh@yahoo.com>
2015-02-05 08:40:30 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -106,19 +106,20 @@ window.onload = function()
     }
     
     //find objects in a Tiled layer that containt a property called "type" equal to a certain value
-      findObjectsByType: function(type, map, layer) {
+   /*
+   findObjectsByType: function(type, map, layer) {
         var result = new Array();
         map.objects[layer].forEach(function(element){
-          if(element.properties.type === type) {
+        if(element.properties.type === type) {
             //Phaser uses top left, Tiled bottom left so we have to adjust the y position
             //also keep in mind that the cup images are a bit smaller than the tile which is 16x16
             //so they might not be placed in the exact pixel position as in Tiled
             element.y -= map.tileHeight;
             result.push(element);
-          }      
+        }      
         });
         return result;
-      },
+    } */
 
     
     function update() 
